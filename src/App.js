2e889e11237f0820f444bbc183989e0506e4eb9c
Sabrina Oumchiche <sabrina.oumchiche@imadrassa.com>
2023-05-15 13:19:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { fetchData } from "./utils/api";
 
-function App() {
+const App = () => {
+  const apiTesting = () => {
+    fetchData("/movie/popular").then((res) => console.log(res));
+  };
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>App</div>
     </div>
   );
-}
+};
 
 export default App;
