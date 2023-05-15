a4851dcad6ce49345d773f836c12f1899946e1ae
Sabrina Oumchiche <sabrina.oumchiche@imadrassa.com>
2023-05-15 14:41:30 -0400

    tested api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMBD_TOKEN = process.env.REACT_APP_TMBD_TOKEN;
+const TMDB_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzNjMTI3MjRlNWEwNmNlY2EwMjNmNjYyZWU0YWUzMiIsInN1YiI6IjYxYTEzNTE0NmY0M2VjMDA0NTE1MTQzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3kheQci00pxmjA0QMqgq_ZcgZyhE9QN2j5nmnukcUnw";
 
 const headers = {
-  Authorization: "Bearer " + TMBD_TOKEN,
+  Authorization: "Bearer " + TMDB_TOKEN,
 };
 
 export const fetchData = async (url, params) => {
