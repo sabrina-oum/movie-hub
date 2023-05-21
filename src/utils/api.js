692deb576089a1becf8222b1a027b2b66def6305
Sabrina Oumchiche <sabrina.oumchiche@imadrassa.com>
2023-05-21 17:30:45 -0400

    added firebase credentials

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,8 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzNjMTI3MjRlNWEwNmNlY2EwMjNmNjYyZWU0YWUzMiIsInN1YiI6IjYxYTEzNTE0NmY0M2VjMDA0NTE1MTQzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3kheQci00pxmjA0QMqgq_ZcgZyhE9QN2j5nmnukcUnw";
 
+const TMDB_TOKEN_TEST = process.env.REACT_APP_TMDB_TOKEN;
+
 const headers = {
   Authorization: "Bearer " + TMDB_TOKEN,
 };
@@ -14,6 +16,7 @@ export const fetchDataFromApi = async (url, params) => {
       headers,
       params,
     });
+    console.log(TMDB_TOKEN_TEST);
 
     return data;
   } catch (err) {
